393f1143fa2e878201bb3eb67230227ebeb26e26
Susmithareddya <93251900+Susmithareddya@users.noreply.github.com>
2021-11-13 16:13:53 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(name='ri20vyju',
+ version='0.1',
+ author='DSSS',
+ author_email='susmitha.arumalla@fau.de',
+ packages=find_packages(),
+ install_requires=['numpy', 'Pillow', 'ipywidgets'])
